8c6099bf248fbf514ee915c274036e7750a750c7
Mark-Waldron <C15501723@mydit.ie>
2021-02-01 12:38:04 +0000

    Killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,36 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+
     public Cat(String name)
     {
         super(name);
     }
+
+    private int numlives;
+
+    public int getnumlives()
+    {
+        return numlives;
+    }
+
+    public void setnumlives(int numlives)
+    {
+        this.numlives = 9;
+    }
+
+    static int kill(int numlives)
+    {
+        while (numlives > 0)
+        {
+            System.out.println("Ouch!\n");
+            numlives--;
+        }
+        if (numlives == 0)
+        {
+            System.out.println("Cat had died\n");
+        }
+    }
+
+    
 }
