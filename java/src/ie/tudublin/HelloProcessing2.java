8d7ef9196e73ed9cf0664f346d2eacb39669d3e7
Mark-Waldron <C15501723@mydit.ie>
2021-02-03 16:04:30 +0000

    Illumanity

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -13,11 +13,36 @@ public class HelloProcessing2 extends PApplet
 	public void setup() {
 		
 	}
+
+	//float x = a;
+	//int i;
+	//String s;
+	//boolean b;
 	
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(255, 0, 0); 
+
+		fill(255, 255, 0);
+		circle(250, 275, 400);
+		
+		fill(0, 255, 255);
+		triangle(250, 50, 40, 450, 460, 450);
+		
+
+		fill(0);
+		ellipse(200, 150, 100, 50); 
+
+		fill(0);
+		ellipse(300, 150, 100, 50); 
+
+		fill(0);
+		circle(250, 300, 135);
+
+		fill(0, 255, 255);
+		circle(250, 275, 135);
+
+		noStroke();
+
 	}
 }
